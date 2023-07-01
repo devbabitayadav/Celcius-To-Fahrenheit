ff4f05d0d9639765d06749544cb63f5da114a058
“devbabitayadav” <babitayadav9584@gmail.com>
2023-07-01 13:42:20 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// program to convert celsius to fahrenheit
+// formula:-f = (c * (9 / 5)) + 32;
+
+const celsius = prompt("Enter a celsius value: ");
+
+// calculate fahrenheit
+
+const fahrenheit = (celsius * 1.8) + 32
+
+// display the result
+
+document.write(`<h2> Result: ${celsius}° celsius is equal to ${fahrenheit}° fahrenheit.`);
